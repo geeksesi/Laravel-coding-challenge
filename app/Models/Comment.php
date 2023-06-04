0d4d9eaf695d14af2b79c1f4a1ef0cf0e8f2d4df
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-06-04 20:20:09 +0330

    #3 feat: Comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ["comment"];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeUserProduct(Builder $query, int $userId, int $productId): Builder
+    {
+        return $query->where("user_id", $userId)->where("product_id", $productId);
+    }
+}
