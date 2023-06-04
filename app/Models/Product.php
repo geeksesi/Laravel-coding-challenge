fd0785b4b9cf353fc8a4977cdfc13f8f2865b0d0
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-06-04 21:15:50 +0330

    #3 feat: comment relation on product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -15,4 +16,9 @@ class Product extends Model
      * @var array<int, string>
      */
     protected $fillable = ["name"];
+
+    public function comment(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
